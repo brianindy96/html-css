3af2e583e861a9b7d265afd98c5577913bcbe0e9
Brian Garton <gartonb@gmail.com>
2023-01-24 20:41:11 +0100

    overlay1Btn clicked and hover gsap styled

--- gsap-landing-page/script.js
+++ gsap-landing-page/script.js
@@ -5,12 +5,20 @@ $(document).ready(function() {
         gsap.to(".overlay1Btn", 1, {
             y: -100,
             opacity: 0,
+            ease: "power4.out",
+
         })
 
         gsap.to(".overlay1BgText", 1, {
             y: -400,
             opacity: 0,
             delay: 0.5,
+            ease: "power2.out",
         })
     })
+
+    $('.overlay1Btn').hover(function(){gsap.to(".overlay1Btn", 0.1, {scale: 1.1})}, function(){gsap.to(".overlay1Btn", 0.1, {scale: 1})})
+
+
+    
 })
